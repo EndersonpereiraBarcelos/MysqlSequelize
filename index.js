abc281063b62fd9378a42a382515c0f88288c801
pbenderson <souza98120@gmail.com>
2021-11-23 08:57:28 -0300

    correção
    add
    update, delete

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
 (async () => {
-
-    const database = require('./db');
-    const Nomes = require('./funcionarios');
-    await database.sync();
-
-    const ListaFuncionarios = await Nomes.create({
-        nome:'enderson',
-        sobrenome:'barcelos',
-        setorTrabalho:'back-end',
-        numeroId:'4444'
-
+    const database = require('./db'); //Passando as config do banco de dados
+    const Produto = require('./produto');
+ 
+    //passando feedback do funcionamento
+    try {
+        const resultado = await database.sync();
+        console.log(resultado);
+    } catch (error) {
+        console.log(error);
+    }
+    
+    //Passando os nomes dos "Produtos da loja"
+    const resultadoCreate = await Produto.create({
+        nome: 'mouse',
+        preco: 10,
+        descricao: 'Um mouse USB bonitão'
     })
 
-    console.log(ListaFuncionarios);
+    /*Esta função findAll permite que a gente passe um
+    objeto de opções por parâmetro para restringir os
+    resultados da nossa consulta também, como mencionado na documentação oficial */
 
-    const Lista = await Nomes.findALL();
-    console.log(Lista);
+    const produtos = await Produto.findAll();
+    console.log(produtos);
+    
+    //DELETAR POR ID
+    Produto.destroy({ where: { id: 1 }}); 
 
+    //UPDATE
+    const produto = await Produto.findByPk(1);
+    //console.log(produto);
+    produto.nome = "Mouse Top";
+ 
+    const resultadoSave = await produto.save();
+    console.log(resultadoSave);
 })();
